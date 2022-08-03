86ac4eb46e92ff587670793d3cbeefd5ab3d7d63
gaponukz <gaponukz54@gmail.com>
2022-08-03 06:41:35 +0300

    Update

--- test.py
+++ test.py
@@ -3,11 +3,13 @@ import itertools
 output_data: list[list[bool]] = []
 metric_data = ['avatar', 'username', 'lastname', 'bio', 'url_in_bio']
 
-for item in itertools.product([0, 1], repeat=5):
-    item = list(item)
-    string = '; '.join([f"{metric_data[i]}{'+' if item[i] else '-'}" for i in range(len(item))]) + " : "
-    item.append(int(bool(input(string))))
-    output_data.append(item)
+
+for _ in range(4):
+    for item in itertools.product([0, 1], repeat=5):
+        item = list(item)
+        string = '; '.join([f"{metric_data[i]}{'🟢' if item[i] else '❌'}" for i in range(len(item))]) + " : "
+        item.append(int(bool(input(string))))
+        output_data.append(item)
 
 for item in output_data:
     print(str(item).replace(', ', ',').replace('[', '').replace(']', ''))
